6d6ee160a6c0c216e4f1f68ffd8fe9c411a5fc78
Alex Clark <gac208@gmail.com>
2023-12-24 19:26:50 -0500

    products data.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import FilterableProductTable from "./components/FilterableProductTable";
-
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
+import PRODUCTS from "./data/data";
 
 export default function App() {
   return <FilterableProductTable products={PRODUCTS} />;
