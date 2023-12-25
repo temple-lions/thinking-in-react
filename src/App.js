175bf86d4ca1575b9fd7e1348fd001ee437f6d47
Alex Clark <gac208@gmail.com>
2023-12-24 19:22:10 -0500

    filtered products

--- src/App.js
+++ src/App.js
@@ -1,27 +1,4 @@
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import ProductTable from "./components/ProductTable";
-
-function FilterableProductTable({ products }) {
-  const [filterText, setFilterText] = useState("");
-  const [inStockOnly, setInStockOnly] = useState(false);
-
-  return (
-    <div>
-      <SearchBar
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-        onFilterTextChange={setFilterText}
-        onInStockOnlyChange={setInStockOnly}
-      />
-      <ProductTable
-        products={products}
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-      />
-    </div>
-  );
-}
+import FilterableProductTable from "./components/FilterableProductTable";
 
 const PRODUCTS = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
